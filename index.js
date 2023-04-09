d702cd09e21891865cc37df5fa3812e68b389020
LiuDi <404637575@qq.com>
2023-04-09 19:56:19 +0800

    jsonserverauth

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const jsonServer = require("json-server") // importing json-server library
 const server = jsonServer.create()
 const router = jsonServer.router("db.json")
 const middlewares = jsonServer.defaults()
+const auth = require("json-server-auth")
 const port = process.env.PORT || 3001 // you can use any port number here; i chose to use 3001
 
+server.db = router.db // add this line before the auth middleware
+server.use(auth)
 server.use(middlewares)
 server.use(router)
 
