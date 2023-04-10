1debafed25acfb82c5344ff15ea7c5c713294de1
LiuDi <404637575@qq.com>
2023-04-10 09:38:39 +0800

    Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -8,8 +8,17 @@ const cors = require("cors")
 const port = process.env.PORT || 3001 // you can use any port number here; i chose to use 3001
 
 server.db = router.db // add this line before the auth middleware
+
 server.use(auth)
 server.use(cors())
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3000")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  next()
+})
 server.use(middlewares)
 server.use(router)
 
